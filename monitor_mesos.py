ea3d452c8750fab0f691feb66208119241a4c888
Matthew Boehm <boehm.matthew@gmail.com>
2017-04-03 12:22:25 -0400

    Add support for mesos auth

--- monitor_mesos.py
+++ monitor_mesos.py
@@ -1,6 +1,14 @@
 """Get metrics from mesos machines and index them in elasticsearch
 
-Usage: [PYTHON_MESOS_ES_<key>=<val>] python monitor_mesos.py <config_file>
+Usage: [PYTHON_MESOS_LOG_LEVEL] [PYTHON_MESOS_USERNAME=<mesos_username>] [PYTHON_MESOS_PASSWORD=<mesos_password>] [PYTHON_MESOS_ES_<key>=<val>] python monitor_mesos.py <config_file>
+
+Environment Variables:
+    PYTHON_MESOS_LOG_LEVEL: python logging level to use (i.e. ERROR, INFO, DEBUG)
+    PYTHON_MESOS_USERNAME: username for authenticating to the mesos API (used for all machines)
+    PYTHON_MESOS_PASSWORD: password for authenticating to the mesos API (used for all machines)
+    PYTHON_MESOS_ES_<key>: override for key/value pair in config["elasticsearch"]
+        For instance, PYTHON_MESOS_ES_password will override config["elasticsearch"]["password"]
+        See the section below for a list of possible keys.
 
 Config file is a json file with the following structure:
     elasticsearch: object representing where to index the data
@@ -13,10 +21,8 @@ Config file is a json file with the following structure:
         name: what to call the machine
         type: "master" or "agent"
         url: base url for mesos on the machine
-
-You can override keys from the elasticsearch config by setting environment variables
-For instance setting PYTHON_MESOS_ES_password will override config["elasticsearch"]["password"]
-This is done so that sensitive information does not need to be stored in the config file
+        username: mesos username for pulling stats (optional)
+        password: mesos password for pulling stats (optional)
 """
 from __future__ import print_function
 import datetime as dt
@@ -60,10 +66,10 @@ def make_metrics_record(machine, metrics, timestamp):
     }
 
 
-def get_machine_metrics(machine_url):
+def get_machine_metrics(machine_url, auth=None):
     """Get the metrics for a mesos machine
     :param machine_url: base url for a mesos instance"""
-    result = requests.get(machine_url + "/metrics/snapshot")
+    result = requests.get(machine_url + "/metrics/snapshot", auth=auth)
     return result.json()
 
 
@@ -71,6 +77,9 @@ def index_machines(config):
     """Get metrics for all the machines listed in the config and index them to elasticsearch"""
     timestamp = dt.datetime.now().isoformat()
     for machine in config["machines"]:
+        auth = None
+        if "username" in machine:
+            auth = (machine["username"], machine["password"])
         metrics = get_machine_metrics(machine["url"])
         record = make_metrics_record(machine, metrics, timestamp)
         logging.debug("Ready to index record %r", record)
@@ -79,16 +88,25 @@ def index_machines(config):
 
 def main():
     """Load the config file, get metrics for all machines, and index results in ES"""
-    logging.basicConfig(level=os.environ.get("MESOS_MONITOR_LOG_LEVEL", "ERROR"))
+    logging.basicConfig(level=os.environ.get("PYTHON_MESOS_LOG_LEVEL", "ERROR"))
     with open(sys.argv[1]) as config_file:
         config = json.load(config_file)
+    logging.info("Loaded config")
+    logging.debug(repr(config))
     config["elasticsearch"].update(
         dict([
             (key[len("PYTHON_MESOS_ES_"):], val) for key, val in os.environ.items()
             if key.startswith("PYTHON_MESOS_ES_")
         ])
     )
-    logging.info("Loaded config")
+    # Set Mesos username/password for all machines
+    for var, key in [("PYTHON_MESOS_USERNAME", "username"), ("PYTHON_MESOS_PASSWORD", "password")]:
+        val = os.environ.get(var)
+        if val:
+            for machine in config["machines"]:
+                machine[key] = val
+
+    logging.info("Updated config with environment variables")
     logging.debug(repr(config))
     index_machines(config)
 
